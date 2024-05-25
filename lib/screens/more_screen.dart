e9508b91db1c9df25faaa3ba0fe0ca6f386f2828
Shahed Noor <shahednoor28@gmail.com>
2024-05-25 09:40:26 +0600

    More screen functionalities and content added

--- lib/screens/more_screen.dart
+++ lib/screens/more_screen.dart
@@ -1,15 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class MoreScreen extends StatefulWidget {
-  const MoreScreen({super.key});
+class MoreScreen extends StatelessWidget {
+  const MoreScreen({Key? key}) : super(key: key);
 
-  @override
-  State<MoreScreen> createState() => _MoreScreenState();
-}
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
-class _MoreScreenState extends State<MoreScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('More'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text('About Developer'),
+            onTap: () => showAboutDialog(
+              context: context,
+              applicationIcon: Image.asset(
+                'assets/images/starter_icon/starter_icon.png',
+                height: 35,
+                width: 35,
+              ),
+              applicationName: 'News Universe',
+              applicationVersion: '1.0.0',
+              applicationLegalese: 'Developed by: Shahed Noor',
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 15),
+                  child: Text(
+                      'This app is developed by Shahed Noor. You can find more about me on my website.'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: TextButton(
+                    onPressed: () {
+                      _launchURL('https://beacons.ai/developernoor');
+                    },
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        foregroundColor: Colors.white),
+                    child: const Text('About Developer'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.privacy_tip),
+            title: const Text('Privacy Policy'),
+            onTap: () => _launchURL(
+                'https://gist.github.com/ShahedNoor/05e7ce9509411f4c9b97073dbc2bc242'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.article),
+            title: const Text('Terms of Conditions'),
+            onTap: () => _launchURL(
+                'https://gist.github.com/ShahedNoor/fd87c667808d6ba04192346f3e0047a9'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.contact_mail),
+            title: const Text('Contact Us'),
+            onTap: () => showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: const Text('Contact Us'),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        _launchURL('mailto:sancbyt@gmail.com');
+                      },
+                      child: ListTile(
+                        leading: Icon(Icons.email),
+                        title: Text('Email: sancbyt@gmail.com'),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        _launchURL('tel:+8801648405873');
+                      },
+                      child: ListTile(
+                        leading: Icon(Icons.phone),
+                        title: Text('Phone: +8801648405873'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const ListTile(
+            leading: Icon(Icons.info),
+            title: Text('App Version'),
+            subtitle: Text('1.0.0'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.star_rate),
+            title: const Text('Rate Us'),
+            onTap: () => _launchURL(
+                'https://play.google.com/store/apps/details?id=com.sancbyt.newsuniverse'),
+          ),
+          ListTile(
+            leading: Icon(Icons.share),
+            title: Text('Share App'),
+            onTap: () {
+              final RenderBox box = context.findRenderObject() as RenderBox;
+              Share.share(
+                  'Check out this awesome app: https://play.google.com/store/apps/details?id=com.sancbyt.newsuniverse',
+                  sharePositionOrigin:
+                      box.localToGlobal(Offset.zero) & box.size);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.help),
+            title: const Text('FAQ'),
+            onTap: () => _launchURL('https://beacons.ai/developernoor'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.book),
+            title: const Text('Licenses'),
+            onTap: () => showLicensePage(context: context),
+          ),
+        ],
+      ),
+    );
   }
 }
